7513665fe1e815ba67f858d24ccee3543f5ea4bb
DRAGON.JPG <132804271+dragonjpg@users.noreply.github.com>
2025-09-05 19:21:04 -0400

    Update morphology-widget.user.js

--- scripts/morphology-widget.user.js
+++ scripts/morphology-widget.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.0
 // @description  adds a share button that generates a widget of the morphology's image with descriptive alt text that links to the morphology parameters in the workshop. utilizes FR's existing "frCopyToClipboard" function
 // @author       dragon.jpg
-// @license      GNU GPLv3
+// @license      MIT
 // @match        https://www1.flightrising.com/scrying/predict*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=flightrising.com
 // @grant        none
