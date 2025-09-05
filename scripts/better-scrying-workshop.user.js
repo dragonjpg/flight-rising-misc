0b9e8a4dacef9a1ff1e2cb8b4e2ac567fe9947d6
DRAGON.JPG <132804271+dragonjpg@users.noreply.github.com>
2025-09-05 19:19:46 -0400

    Update better-scrying-workshop.user.js

--- scripts/better-scrying-workshop.user.js
+++ scripts/better-scrying-workshop.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Flight Rising: Better Scrying Workshop
 // @namespace   https://github.com/dragonjpg/
+// @author      dragon.jpg
+// @description based on a script by zombae (https://greasyfork.org/en/scripts/423730). adds next/previous buttons for each dropdown, the option to auto-scry on dropdown change, and a button to quickly copy a BBCode widget of the morphology image that links to its parameters. 
 // @match       https://www1.flightrising.com/scrying/predict*
 // @grant       none
 // @version     1.0.0
-// @author      dragonjpg
-// @description based on a script by zombae (https://greasyfork.org/en/scripts/423730). adds next/previous buttons, the option to auto-scry on dropdown change, and a button to quickly copy a BBCode widget of the morphology and its parameters. 
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=flightrising.com
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=flightrising.com
 // ==/UserScript==
 
 (function() {
