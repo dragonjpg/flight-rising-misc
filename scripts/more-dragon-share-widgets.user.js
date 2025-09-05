adc8a41fb272956c4fc62b8cae99ef91dadc36df
DRAGON.JPG <132804271+dragonjpg@users.noreply.github.com>
2025-09-05 19:22:36 -0400

    Update more-dragon-share-widgets.user.js

--- scripts/more-dragon-share-widgets.user.js
+++ scripts/more-dragon-share-widgets.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Flight Rising: More Dragon Share Widgets
-// @namespace   https://github.com/dragonjpg
+// @namespace    https://github.com/dragonjpg
 // @version      1.0.2
 // @description  adds more share widgets and an alt tag of the dragon's name and id to the original full-size widget. utilizes FR's existing "frCopyToClipboard" function
 // @author       dragon.jpg
