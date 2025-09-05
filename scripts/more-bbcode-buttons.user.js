911e09420d342b8c799b1f6875c614485c8488f5
DRAGON.JPG <132804271+dragonjpg@users.noreply.github.com>
2025-09-05 19:21:22 -0400

    Update more-bbcode-buttons.user.js

--- scripts/more-bbcode-buttons.user.js
+++ scripts/more-bbcode-buttons.user.js
@@ -9,6 +9,7 @@
 // @match       https://*.flightrising.com/msgs/*
 // @grant       none
 // @version     1.0.0
+// @license     MIT
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=flightrising.com
 // ==/UserScript==
 
