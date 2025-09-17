69768a99de6409ec21461917371819a2d3cdfeac
DRAGON.JPG <132804271+dragonjpg@users.noreply.github.com>
2025-09-17 17:27:51 -0400

    Update more-bbcode-buttons.user.js
    Needed to bump ver number for the minor fix.

--- scripts/more-bbcode-buttons.user.js
+++ scripts/more-bbcode-buttons.user.js
@@ -8,7 +8,7 @@
 // @match       https://*.flightrising.com/forums*
 // @match       https://*.flightrising.com/msgs/*
 // @grant       none
-// @version     1.0.0
+// @version     1.0.1
 // @license     MIT
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=flightrising.com
 // ==/UserScript==
